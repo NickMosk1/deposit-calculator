1349ec2dc0875887a255e9f5d4d03986f2f699c3
moskvichev <moskvichev@keeneye.pro>
2026-02-03 16:50:16 +0400

    front service fix

--- frontend/src/services/calculation.service.ts
+++ frontend/src/services/calculation.service.ts
@@ -13,7 +13,7 @@ class CalculationService {
 
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.VITE_API_URL,
+      baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080/api',
       headers: { 'Content-Type': 'application/json' },
     });
   };
